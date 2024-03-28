8dadf0843aa57045d4c39a2acf14f4ba4365ad2c
COGMLS <mlsmatheus@hotmail.com>
2024-03-28 20:52:05 -0300

    Fix missing complete user command line on console.

--- FontMng/main.cpp
+++ FontMng/main.cpp
@@ -1,31 +1,60 @@
 #include "pch.h"
 
 #include "fontPack.hpp"
+#include <vector>
 
 int main(int argc, const char* argv[])
 {
     std::locale loc("en_US.UTF-8");
-
     std::locale::global(loc);
+
+    std::vector<fontPack> list;
 
     std::wcout << "Font Package Manager for Linux\n";
 
     std::wstring usr = L"";
 
-    while (usr != L"exit")
+    std::wcout << L"Use \"help\" for details. >";
+    while (std::getline(std::wcin, usr))
     {
-        usr = L"";
-        std::wcout << L"Use \"help\" for details. >";
-        std::wcin >> usr;
+        if (usr == L"exit")
+        {
+            break;
+        }
 
         if (usr == L"hi")
         {
             std::wcout << "Say hi!" << std::endl;
         }
+        else if (usr.starts_with(L"add "))
+        {
+            usr.erase(0, std::wstring(L"add ").size());
+
+            if (usr.starts_with(L'\"') || usr.starts_with(L'\''))
+            {
+                usr.erase(0, 1);
+            }
+            
+            if (usr.ends_with(L'\"') || usr.ends_with(L'\''))
+            {
+                usr.erase(usr.size() - 1, 1);
+            }
+            
+            std::wstring tmp = usr;
+            
+            std::wcout << L"Adding path:" << tmp << std::endl;
+
+            std::filesystem::path p(tmp);
+
+            std::wcout << L"Path exists: " << std::filesystem::exists(p) << std::endl;
+        }
         else
         {
             std::wcout << L"Command not recognized! Make sure the command is only in lower case." << std::endl;
         }
+
+        std::wcout << L"Use \"help\" for details. >";
+        usr = L"";
     }
 
     return 0;
