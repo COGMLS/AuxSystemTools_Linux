12c2472af069855fb56bbb62a3f0ed7f59055da6
COGMLS <mlsmatheus@hotmail.com>
2024-03-28 20:17:56 -0300

    Add constant for local user installation. Add constant for "remove" label.

--- FontMng/constatnts.hpp
+++ FontMng/constatnts.hpp
@@ -3,8 +3,10 @@
 #ifndef CONSTANTS_HPP
 #define CONSTANTS_HPP
 
+#define USR_FONT_PATH_W L"~/.local/share/fonts"
 #define SYS_FONT_PATH_W L"/usr/share/fonts"
 #define USR_INSTALL_W L"User"
 #define SYS_INSTALL_W L"System"
+#define REM_INSTALL_W L"Remove"
 
 #endif // !CONSTANTS_HPP
