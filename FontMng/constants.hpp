03db1b9797d36dbc6c2ad2e19c147236e1674850
COGMLS <mlsmatheus@hotmail.com>
2024-08-05 21:38:24 -0300

    Fix the file's name. Added new constants entries.

---
+++ FontMng/constants.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+#define USR_FONT_PATH_W 	L"~/.local/share/fonts"
+#define SYS_FONT_PATH_W 	L"/usr/share/fonts"
+#define USR_INSTALL_W 		L"User"
+#define SYS_INSTALL_W 		L"System"
+#define REM_INSTALL_W 		L"Remove"
+
+#define SPACE_W				L' '
+#define QUOTE_W				L'"'
+
+#endif // !CONSTANTS_HPP
