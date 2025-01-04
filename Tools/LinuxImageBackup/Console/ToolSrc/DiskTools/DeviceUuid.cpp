c8496be3a2c627b68620413ab64f5014f9ffdef8
COGMLS <mlsmatheus@hotmail.com>
2025-01-04 16:43:09 -0300

    Added DeviceUuid.hpp and DeviceUuid.cpp

---
+++ Tools/LinuxImageBackup/Console/ToolSrc/DiskTools/DeviceUuid.cpp
@@ -0,0 +1,159 @@
+#include "DeviceUuid.hpp"
+
+std::string uuidArr2Str(std::vector<std::string> uuid, bool noDashes)
+{
+    std::string uuid_str;
+
+	for (size_t i = 0; i < uuid.size(); i++)
+	{
+		if (!noDashes)
+		{
+			if (i + 1 < uuid.size())
+			{
+				uuid_str += uuid[i] + "-";
+			}
+			else
+			{
+				uuid_str += uuid[i];
+			}
+		}
+		else
+		{
+			uuid_str += uuid[i];
+		}
+	}
+
+	return uuid_str;
+}
+
+void uuidStr2lower(std::string &str)
+{
+	for (size_t i = 0; i < str.size(); i++)
+	{
+		if (std::isupper(str[i]))
+		{
+			str[i] = std::tolower(str[i]);
+		}
+	}
+}
+
+DeviceUuid::DeviceUuid(std::string uuid_str)
+{
+	uuidStr2lower(uuid_str);
+
+	char* c_uuid_str = nullptr;
+	std::strcpy(c_uuid_str, uuid_str.c_str());
+	const char* delim = "-";
+	char* token = std::strtok(c_uuid_str, delim);
+
+	while (token)
+	{
+		if (token != nullptr)
+		{
+			this->uuid.push_back(token);
+		}
+		token = std::strtok(nullptr, delim);
+	}
+}
+
+DeviceUuid::DeviceUuid(std::filesystem::path uuid_device_path)
+{
+	if (std::filesystem::is_block_file(uuid_device_path))
+	{
+		std::filesystem::path device_path = std::filesystem::read_symlink(uuid_device_path);
+		this->device_name = device_path.stem();
+
+		std::string uuid_str = uuid_device_path.stem();
+
+		uuidStr2lower(uuid_str);
+
+		char* c_uuid_str = nullptr;
+		std::strcpy(c_uuid_str, uuid_str.c_str());
+		const char* delim = "-";
+		char* token = std::strtok(c_uuid_str, delim);
+
+		while (token)
+		{
+			if (token != nullptr)
+			{
+				this->uuid.push_back(token);
+			}
+			token = std::strtok(nullptr, delim);
+		}
+	}
+}
+
+DeviceUuid::DeviceUuid(const DeviceUuid &other)
+{
+	this->uuid = other.uuid;
+	this->device_name = other.device_name;
+}
+
+DeviceUuid::DeviceUuid(DeviceUuid &&other) noexcept
+{
+	this->uuid = std::move(other.uuid);
+	this->device_name = std::move(other.device_name);
+}
+
+DeviceUuid::~DeviceUuid()
+{
+}
+
+std::string DeviceUuid::to_string(bool noDashes)
+{
+	return uuidArr2Str(this->uuid, noDashes);
+}
+
+std::string DeviceUuid::device()
+{
+    return this->device_name;
+}
+
+bool DeviceUuid::operator==(const DeviceUuid &other)
+{
+    return this->to_string() == uuidArr2Str(other.uuid, false);
+}
+
+bool DeviceUuid::operator!=(const DeviceUuid &other)
+{
+    return !(*this == other);
+}
+
+bool DeviceUuid::operator==(const std::string &uuid_str)
+{
+    bool hasDashes = false;
+
+	for (size_t i = 0; i < uuid_str.size(); i++)
+	{
+		if (uuid_str[i] == '-')
+		{
+			hasDashes = true;
+			break;
+		}
+	}
+
+	std::string tmp = uuid_str;
+
+	uuidStr2lower(tmp);
+
+	return this->to_string(!hasDashes) == tmp;
+}
+
+DeviceUuid &DeviceUuid::operator=(const DeviceUuid &other)
+{
+    this->uuid = other.uuid;
+	this->device_name = other.device_name;
+	return *this;
+}
+
+DeviceUuid &DeviceUuid::operator=(DeviceUuid &&other) noexcept
+{
+	if (this == &other)
+	{
+		return *this;
+	}
+
+	this->uuid = other.uuid;
+	this->device_name = other.device_name;
+	return *this;
+}
---
+++ Tools/LinuxImageBackup/Console/ToolSrc/DiskTools/DeviceUuid.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+#ifndef DEVICE_UUID_HPP
+#define DEVICE_UUID_HPP
+
+#include <string>
+#include <cstring>
+#include <cctype>
+#include <vector>
+#include <filesystem>
+
+/**
+ * @brief Detect and holds a Device UUID
+ * @note This class does not check if the UUID is valid or not. Only holds the given data and process the segments into a vector, removing the dashes.
+ */
+class DeviceUuid
+{
+	private:
+
+		std::vector<std::string> uuid;
+		std::string device_name;
+
+	public:
+
+		/**
+		 * @brief Store a Device UUID based on given string
+		 * @param uuid_str UUID string
+		 */
+		DeviceUuid (std::string uuid_str);
+		
+		/**
+		 * @brief Search for a block device and holds the UUID and the reference to the device
+		 * @param uuid_device_path Path to a device block. I.e. /dev/disk/by-uuid/sda1
+		 */
+		DeviceUuid (std::filesystem::path uuid_device_path);
+
+		DeviceUuid (const DeviceUuid& other);
+
+		DeviceUuid (DeviceUuid&& other) noexcept;
+
+		~DeviceUuid();
+
+		/**
+		 * @brief Transform the UUID segments stored in the vector as a string value
+		 * @param noDashes If true, no dashes will be added
+		 * @return Return a UUID string value
+		 * @note The UUID string characters are converted to lowercase.
+		 */
+		std::string to_string (bool noDashes = false);
+
+		/**
+		 * @brief Get the device associated with the UUID, if the object was created with a valid block device path
+		 * @return If valid a string with the name to the device will return
+		 */
+		std::string device();
+
+		bool operator== (const DeviceUuid& other);
+
+		bool operator!= (const DeviceUuid& other);
+
+		bool operator== (const std::string& uuid_str);
+
+		DeviceUuid& operator= (const DeviceUuid& other);
+
+		DeviceUuid& operator= (DeviceUuid&& other) noexcept;
+};
+
+std::string uuidArr2Str (std::vector<std::string> uuid, bool noDashes);
+
+void uuidStr2lower (std::string& str);
+
+#endif // !DEVICE_UUID_HPP
